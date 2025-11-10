b88f2de3efa185cbc61d25910472f60440fcc8b3
Leanne  Thoppil <leannelthoppil@gmail.com>
2025-11-10 11:07:46 -0600

    first

---
+++ compover.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <vector>
+#include <string>
+
+
+class Complex {
+    private: 
+    double real;
+    double imaginary;
+
+    public:
+    Complex(double r = 0.0, double i = 0.0) : real(r), imaginary(i) {}
+}
+int main ();
+
+
+
